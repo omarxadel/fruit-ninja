eb9806049c3d2eaa0077dd658324921d705828e9
Omar Adel <48330931+omarxadel@users.noreply.github.com>
2019-05-08 17:16:41 +0200

    Control Package

---
+++ src/Control/GameActions.java
@@ -0,0 +1,12 @@
+package Control;
+
+import model.GameObject;
+
+public interface GameActions {
+	public GameObject createGameObject ();
+	public void updateObjectsLocations ();
+	public void sliceObjects ();
+	public void saveGame ();
+	public void loadGame ();
+	public void ResetGame ();
+}
