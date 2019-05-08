3ebbf6e5252199c66062d426aadcf264f9495c46
omarxadel <48330931+omarxadel@users.noreply.github.com>
2019-05-09 00:50:33 +0200

    DocumentationCommit
    To keep changes

--- src/controller/Context.java
+++ src/controller/Context.java
@@ -3,28 +3,28 @@ package controller;
 import model.GameObject;
 
 public class Context {
-GameActions gameActions;
+	GameActions gameActions;
 
-public Context(GameActions gameActions) {
-	this.gameActions=gameActions;
-}
-
-public GameObject createGameObject () {
-	return null;
-}
-public void updateObjectsLocations () {
+	public Context(GameActions gameActions) {
+		this.gameActions=gameActions;
+	}
 	
-}
-public void sliceObjects () {
-	
-}
-public void saveGame () {
-	
-}
-public void loadGame () {
-	
-}
-public void ResetGame () {
-	
-}
-}
+	public GameObject createGameObject () {
+		return gameActions.createGameObject();
+	}
+	public void updateObjectsLocations () {
+		
+	}
+	public void sliceObjects () {
+		
+	}
+	public void saveGame () {
+		
+	}
+	public void loadGame () {
+		
+	}
+	public void ResetGame () {
+		
+	}
+	}
--- src/controller/Level1.java
+++ src/controller/Level1.java
@@ -1,12 +1,28 @@
 package controller;
 
+import model.Fruit;
 import model.GameObject;
 
 public class Level1 implements GameActions{
+	private ObjectFactory factory = new ObjectFactory();
 
 	@Override
-	public GameObject createGameObject() {
-		// TODO Auto-generated method stub
+	public GameObject createGameObject() {     	
+        	int shows = (int) (Math.random()*7);
+        	switch(shows) {
+        	case 1 : 
+        		return factory.getObject("APPLE");
+        	case 2 :
+        		return factory.getObject("BANANA");
+        	case 3 :
+        		return factory.getObject("STRAWBERRY");
+        	case 4 :
+        		return factory.getObject("WATERMELON");
+        	case 5 : 
+        		return factory.getObject("PEACH");
+        	case 6 :
+        		return factory.getObject("FATALBOMB");
+        	}
 		return null;
 	}
 
