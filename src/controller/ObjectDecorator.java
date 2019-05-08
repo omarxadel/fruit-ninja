3493870a27386257714e149f7f3499b53ae3c055
hodaamr <48572265+hodaamr@users.noreply.github.com>
2019-05-08 22:56:17 +0200

    Update ObjectDecorator.java

--- src/controller/ObjectDecorator.java
+++ src/controller/ObjectDecorator.java
@@ -3,16 +3,14 @@ package controller;
 import model.Fruit;
 import model.GameObject;
 
-public abstract class ObjectDecorator {
+public abstract class ObjectDecorator implements GameObject {
 	
 	private GameObject FruitDecorator;
 
-	public ObjectDecorator(GameObject fruitDecorator) {
+	public ObjectDecorator(GameObject FruitDecorator) {
 		super();
-		FruitDecorator = fruitDecorator;
+		this.FruitDecorator=FruitDecorator;
 	}
-	
-	
 	
 	
 
